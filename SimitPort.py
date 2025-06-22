b1219ce9609e614f1f25efc2c06d3bde0334dab6
simit <aslansemihbiyikli@gmail.com>
2025-06-22 13:18:16 +0200

    Update SimitPort.py

--- SimitPort.py
+++ SimitPort.py
@@ -5,23 +5,26 @@ from colorama import Fore, init
 
 
 
-init(autoreset=True)
+print(Fore.BLUE + """
+   ▄████████  ▄█  ███▄▄▄▄      ▄██████▄   ▄█          ▄████████    ▄███████▄  ▄██████▄     ▄████████     ███        ▄████████    ▄████████ 
+  ███    ███ ███  ███▀▀▀██▄   ███    ███ ███         ███    ███   ███    ███ ███    ███   ███    ███ ▀█████████▄   ███    ███   ███    ███ 
+  ███    █▀  ███▌ ███   ███   ███    █▀  ███         ███    █▀    ███    ███ ███    ███   ███    ███    ▀███▀▀██   ███    █▀    ███    ███ 
+  ███        ███▌ ███   ███  ▄███        ███        ▄███▄▄▄       ███    ███ ███    ███  ▄███▄▄▄▄██▀     ███   ▀  ▄███▄▄▄      ▄███▄▄▄▄██▀ 
+▀███████████ ███▌ ███   ███ ▀▀███ ████▄  ███       ▀▀███▀▀▀     ▀█████████▀  ███    ███ ▀▀███▀▀▀▀▀       ███     ▀▀███▀▀▀     ▀▀███▀▀▀▀▀   
+         ███ ███  ███   ███   ███    ███ ███         ███    █▄    ███        ███    ███ ▀███████████     ███       ███    █▄  ▀███████████ 
+   ▄█    ███ ███  ███   ███   ███    ███ ███▌    ▄   ███    ███   ███        ███    ███   ███    ███     ███       ███    ███   ███    ███ 
+ ▄████████▀  █▀    ▀█   █▀    ████████▀  █████▄▄██   ██████████  ▄████▀       ▀██████▀    ███    ███    ▄████▀     ██████████   ███    ███ 
+                                         ▀                                                ███    ███                            ███    ███         
+            """)
+        print(Fore.LIGHTBLUE_EX + "THIS PORT TESTER WAS MADE BY @REDSIMIT")
+        ip = input(Fore.GREEN + "IP to scan: ")
+        port = int(input(Fore.GREEN + "Input the following port: "))
+        time.sleep(1)
+        print(Fore.LIGHTGREEN_EX + "Connecting...")
+        time.sleep(2)
 
-print(Fore.RED + "THIS PORT TESTER WAS MADE BY @REDSIMIT")
-ip = input( Fore.GREEN + "Input the following ip adress: ")
-port = int(input(Fore.GREEN + "Input the following port: "))
-time.sleep(1)
-print(Fore.LIGHTGREEN_EX + "Connecting...")
-time.sleep(2)
-
-try:
-    socket.create_connection((ip, port), timeout=3)
-    print(Fore.GREEN + "port is open")
-except socket.error:
-    print(Fore.GREEN + "port is closed")
-print(Fore.RED + "More Tools in https://Redsimit.netlify.app")
-
-
-
-
-
+        try:
+            socket.create_connection((ip, port), timeout=4)  # you can change the timeout if you want cuz its kinda long
+            print(Fore.GREEN + "port is open")
+        except socket.error:
+            print(Fore.GREEN + "port is closed")
